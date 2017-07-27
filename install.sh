b0f0e7c2d086adce9464de5a33c00194def36d39
mooccat <825681860@qq.com>
2017-07-27 23:04:10 +0800

    change ssr location
    change ssr location

--- install.sh
+++ install.sh
@@ -59,7 +59,7 @@ fi
 
 #Install SSR and SSR-Bash
 cd /usr/local
-git clone https://github.com/shadowsocksr/shadowsocksr.git
+git clone https://github.com/mooccat/shadowsocksr.git
 git clone https://github.com/mooccat/SSR-Bash-Python.git
 cd /usr/local/shadowsocksr
 bash initcfg.sh
